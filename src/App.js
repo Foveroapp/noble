ff07c194dfbe622717f9fc4dbe8b7f6a833d1113
Dayo Banjo <banjodayo39@gmail.com>
2024-01-10 06:43:14 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+  
+        <a
+          className="Testers Link"
+          href="https://testflight.apple.com/join/2JUZtycA"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           Noble App Download
+        </a>
+      </header>
+      <main>
+        <img src="/phone.png" alt="Example" style={{ width: '20%' }} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
